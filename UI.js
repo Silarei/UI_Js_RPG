4862768310ba990a7a32a4f4818dd5fc1d030e8d
CARION Baptiste <baptiste.carion@sfr.fr>
2021-12-08 19:01:46 +0100

    update 08/12

--- UI.js
+++ UI.js
@@ -1,13 +1,18 @@
 var blueTemplarHP;
 var blueTemplarPA;
+var blueTemplarAttack;
+var blueTemplarStunned;
 var redTemplarHP;
 var redTemplarPA;
+var redTemplarAttack;
+var redTemplarStunned;
 var greenTemplarHP;
 var greenTemplarPA;
 var blackTemplarHP;
 var blackTemplarPA;
 var witchHP;
-var blueTemplarAttack;
+var compteurRound;
+
 
 //Fonction d'initialisation des variables
 function initialisation() {
@@ -19,14 +24,60 @@ function initialisation() {
     greenTemplarPA = 30;
     blackTemplarHP = 40;
     blackTemplarPA = 30;
-    witchHP = 60;
-    blueTemplarAttack = 8;
+    witchHP = 80;
+    blueTemplarAttack = 10;
+    blueTemplarStunned = false;   
+    redTemplarAttack = 15;
+    redTemplarStunned = false;
     nbTour = 1;
+    compteurRound = 0;
 }
 
 function round() {
-    witchHP = witchHP - blueTemplarAttack;
-    document.getElementById("combatLog").innerHTML = "Le templier inflige " + blueTemplarAttack + "dégâts !<br> Il reste a la sorcière " + witchHP + " PV !<br> <input type='button' onclick='witchStrikeBack()' value='NEXT'>";
+    if (compteurRound == 0) {
+        if (!blueTemplarStunned && blueTemplarHP > 0) {
+            tourTemplier("bleu", blueTemplarAttack);    
+        }
+        else if (blueTemplarHP <= 0) {
+            document.getElementById("combatLog").innerHTML = "Le templier bleu est mort et ne peux plus attaquer !";
+        }
+        else {
+            document.getElementById("combatLog").innerHTML = "Le templier bleu est incapable d'attaquer ce tour !";
+        }
+    }
+    else if (compteurRound == 1) {
+        if (!redTemplarStunned && redTemplarHP > 0) {
+            tourTemplier("rouge", redTemplarAttack);
+        }
+        else if (redTemplarHP <= 0) {
+            document.getElementById("combatLog").innerHTML = "Le templier rouge est mort et ne peux plus attaquer !";
+        }
+        else {
+            document.getElementById("combatLog").innerHTML = "Le templier rouge est incapable d'attaquer ce tour !";
+        }
+    }
+}
+
+function tourTemplier(templarName, templarAttack) {
+    document.getElementById("combatLog").innerHTML = "Le templier " + templarName + " se prépare à agir ! <br> <input type='button' onclick='templarAttackMonster(\""+templarName+"\","+templarAttack+")' value='Attaque'> <input type='button' onclick='templarDefend("+templarName+")' value='Defense'> <input type='button' onclick='templarSpecialAttack("+templarName+")' value='Attaque Speciale'> ";
+}
+
+function templarAttackMonster(templarName, templarAttack) {
+    witchHP = witchHP - templarAttack;
+    document.getElementById("combatLog").innerHTML = "Le templier " + templarName + " inflige " + templarAttack + "dégâts !<br> Il reste a la sorcière " + witchHP + " PV !<br> <input type='button' onclick='increaseCounter()' value='NEXT'>";
+}
+
+function templarDefend(templarName) {
+    increaseCounter()
+}
+
+function templarSpecialAttack(templarName) {
+    increaseCounter()
+}
+
+function increaseCounter() {
+    compteurRound = compteurRound + 1;
+    returnToStartingState()
 }
 
 function witchStrikeBack() {
